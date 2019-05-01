85abc02cc3ace609b59a8ec9911b8dc75bb9d436
Thibaut Lienart <tlienart@me.com>
2019-05-01 15:54:54 +1000

    draft site

---
+++ deploy.js
@@ -0,0 +1,3 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish('docs/build', function(err) {});
---
+++ docs/make.jl
@@ -0,0 +1,65 @@
+using JuDocTemplates, JuDoc
+
+build = joinpath(@__DIR__, "build")
+
+function fixdir(τ::String)
+    # 1. remove src folder (not needed anymore)
+    rm(joinpath(τ, "src"), recursive=true)
+    # 2. fix links in index.html etc
+    html_files = String[]
+    for (root, _, files) ∈ walkdir(τ)
+        for file ∈ files
+            endswith(file, ".html") || continue
+            fp   = joinpath(root, file)
+            html = read(fp, String)
+            html = replace(html, r"href=\"\/" => "href=\"/templates/$τ/")
+            write(fp, html)
+        end
+    end
+    return
+end
+
+# make a template folder with a subfolder for each template
+# compile each template with a fullpass of judoc
+begin
+    # first clean up the directory to avoid clashes etc
+    begin
+        cd(build)
+        isdir("templates") && rm("templates", recursive=true)
+        cd("..")
+    end
+    # make the template folder
+    templates = mkpath(joinpath(build, "templates"))
+    cd(templates)
+    for τ ∈ JuDocTemplates.LIST_OF_TEMPLATES
+        newsite(τ; template=τ, changedir=true, verbose=false)
+        serve(; clear=true, verb=false, single=true, prerender=false)
+        cd("..")
+        fixdir(τ)
+    end
+end
+
+# build the index page
+begin
+    html = IOBuffer()
+    write(html, read(joinpath(@__DIR__, "index_head.html"), String))
+
+    # One card per template
+    for τ ∈ JuDocTemplates.LIST_OF_TEMPLATES
+        c = """
+            <a href="/templates/$τ/index.html">
+            <div class="card">
+              <div class="descr">
+                <h2>$τ</h2>
+              </div>
+              <div class="code">
+                <pre><font color="green" style="font-weight:bold">julia></font> <font color="royalblue">newsite</font>(<font color="darkred">"MySite"</font>; template=<font color="darkred">"$τ"</font>)</pre>
+              </div>
+            </div>
+            </a>
+            """
+        write(html, c)
+    end
+    write(html, read(joinpath(@__DIR__, "index_foot.html"), String))
+    write(joinpath(build, "index.html"), take!(html))
+end
