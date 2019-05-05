01c764e81390068569078b66bea66f9f0357048f
Thibaut Lienart <ltib@me.com>
2019-05-05 10:43:42 +1000

    lanyon ongoing

--- src/JuDocTemplates.jl
+++ src/JuDocTemplates.jl
@@ -4,7 +4,7 @@ export newsite
 
 const JUDOC_PATH = splitdir(pathof(JuDocTemplates))[1] # .../JuDocTemplates/src
 const TEMPL_PATH = joinpath(JUDOC_PATH, "templates")
-const LIST_OF_TEMPLATES = ("basic", "pure-sm", "vela", "hypertext", "tufte", "hyde")
+const LIST_OF_TEMPLATES = ("basic", "pure-sm", "vela", "hypertext", "tufte", "hyde", "lanyon")
 
 """
     newsite(topdir; template="basic", cd=true)
